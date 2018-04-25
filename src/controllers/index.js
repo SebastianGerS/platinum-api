9d5ea231072282bbcd424207191220c70125aa3a
Izabel Rosén <izabel.rosen@chasacademy.se>
2018-04-25 15:24:13 +0200

    Export questionnaires from the central map of controllers

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,9 +1,11 @@
 import Sessions from './Sessions';
 import Tests from './Tests';
 import Users from './Users';
+import Questionnaires from './Questionnaires';
 
 export default {
   Sessions,
   Tests,
   Users,
+  Questionnaires,
 };
