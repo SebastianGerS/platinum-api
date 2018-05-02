f3448a06fba772454288b4face866d268b9727d7
´SebastianGerSe <sebastian.gerstel.sollerman@chasacademy.se>
2018-05-02 21:54:41 +0200

    add create methode for questions

--- src/lib/Questions.js
+++ src/lib/Questions.js
@@ -1,5 +1,6 @@
+import * as DB from '../models';
 import * as Options from './Options';
-
+/* eslint-disable consistent-return, no-console */
 export function jsonQuestion(Question) {
   return {
     id: Question.id,
@@ -16,3 +17,51 @@ export function jsonQuestions(Questions) {
     .map(Question => jsonQuestion(Question));
 }
 
+export function create(data) {
+  const {
+    res, userId, body, returnData,
+  } = data;
+
+  const {
+    name, questionnaireId, type, order, options,
+  } = body;
+
+  DB.Questionnaire
+    .findById(questionnaireId)
+    .then((Questionnaire) => {
+      if (!Questionnaire) { return res.status(400).send({ message: 'The questionnaire you tried to create questions to does not exist' }); }
+      if (Questionnaire.userId !== userId) { return res.status(401).send({ message: 'You can\'t create questions for questionnaire that are not yours' }); }
+
+      DB.Question
+        .create({
+          name,
+          questionnaireId,
+          type,
+          order,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        })
+        .then((Question) => {
+          if (!Question) { return res.status(400).send({ message: 'Question could not be created' }); }
+          if (!options) { return res.status(200).send({ message: 'new Question was created' }); }
+          Options.create({
+            res,
+            userId,
+            questionId: Question.id,
+            questionnaireId,
+            options,
+            returnData,
+          });
+        }).then(Question => (returnData ? Question : res.status(200).send({ message: 'Successfully created a new question with options!' })))
+        .catch((error) => {
+          console.log(error);
+
+          return res.status(400).send(error);
+        });
+    })
+    .catch((error) => {
+      console.log(error);
+
+      return res.status(400).send(error);
+    });
+}
