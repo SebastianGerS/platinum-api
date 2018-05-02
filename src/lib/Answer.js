80566fec368cb6f0fcf1c7b8e6f251cb74dca34f
jonastore <jonas.toremar@chasacademy.se>
2018-05-02 15:18:48 +0200

    remove comments

--- src/lib/Answer.js
+++ src/lib/Answer.js
@@ -33,43 +33,6 @@ export function list(options) {
     });
 }
 
-export function findOne(options) {
-  const { res, returnData, query } = options;
-
-  return DB.Answer
-    .findOne({
-      where: query,
-      include: [{
-        model: DB.Vote,
-        as: 'Votes',
-        separate: true,
-        order: [['order', 'ASC']],
-        include: [{
-          model: DB.Option,
-          as: 'Options',
-          separate: true,
-          order: [['order', 'ASC']],
-        }],
-      }],
-    })
-    .then((Answer) => {
-      'something was found??';
-
-      const json = Answer ? jsonAnswer(Answer) : null;
-
-      if (returnData) return { object: Answer, json };
-
-      return res.status(Answer ? 200 : 404).json(json);
-    })
-    .catch((error) => {
-      console.log(error);
-
-      return returnData ? error : res.status(400).send(error);
-    });
-}
-
-// create
-// få in poll id, skicka tillbaka det som skapats .then/ ta emot pollId
 export function create(options) {
   const { res, body } = options;
   const {
