c433886ca934228bbc4c27a6e69eefd285c09d98
´SebastianGerSe <sebastian.gerstel.sollerman@chasacademy.se>
2018-05-08 17:14:42 +0200

    Add socket.io to server
    	* add lib functions for polls
    	* add socket listener for /my-vote
    	* add socket emit with poll-data for /polls/:pollId listeners on /my-vote trigger

--- src/server.js
+++ src/server.js
@@ -2,6 +2,9 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import routes from './routes';
+import http from 'http';
+import socket from 'socket.io';
+import * as Polls from './lib/Polls';
 
 /* eslint-disable no-unused-vars, no-sequences, no-unused-expressions,
 no-use-before-define, no-param-reassign, no-console, func-names, global-require */
@@ -21,10 +24,13 @@ if (!process.env.PORT) {
 }
 
 const app = express();
+const server = http.Server(app);
+const io = socket(server);
+
 const port = process.env.PORT || 7770;
 const allowOrigin = process.env.ALLOW_ORIGIN || '*';
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`[api][listen] http://localhost:${port}`);
 });
 
@@ -42,3 +48,16 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 
 routes(app);
+
+io.on('connection', (client) => {
+  client.on('/my-vote', (pollId) => {
+    Polls.findOne({
+      where: {
+        id: pollId,
+      },
+      returnData: true,
+    }).then((res) => {
+      client.broadcast.emit(`/polls/${pollId}`, res);
+    });
+  });
+});
